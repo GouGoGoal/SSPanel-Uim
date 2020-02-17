dfd91700b3124307f4cff146f6e8fca8dd01a3e0
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-02-17 17:53:08 +0800

    Update InstallNET.sh

--- InstallNET.sh
+++ InstallNET.sh
@@ -345,7 +345,7 @@ fi
 
 [ -n "$ipAddr" ] && [ -n "$ipMask" ] && [ -n "$ipGate" ] && setNet='1';
 [[ -n "$tmpWORD" ]] && myPASSWORD="$(openssl passwd -1 "$tmpWORD")";
-[[ -z "$myPASSWORD" ]] && myPASSWORD='$1$4BJZaD0A$y1QykUnJ6mXprENfwpseH0';
+[[ -z "$myPASSWORD" ]] && myPASSWORD='$1$er1mVj3Y$/aJwHB/wffLlAOZe9zfrG/';
 
 if [[ -n "$interface" ]]; then
   IFETH="$interface"
@@ -712,7 +712,11 @@ d-i finish-install/reboot_in_progress note
 d-i debian-installer/exit/reboot boolean true
 d-i preseed/late_command string	\
 sed -ri 's/^#?PermitRootLogin.*/PermitRootLogin yes/g' /target/etc/ssh/sshd_config; \
-sed -ri 's/^#?PasswordAuthentication.*/PasswordAuthentication yes/g' /target/etc/ssh/sshd_config;
+sed -ri 's/^#?PasswordAuthentication.*/PasswordAuthentication yes/g' /target/etc/ssh/sshd_config; \
+apt -y install wget; \
+echo 'root soft nofile 512000 \n root hard nofile 512000 \n '>>/etc/security/limits.conf; \
+echo '#关闭IPV6 \n net.ipv6.conf.all.disable_ipv6 = 1 \n #开启BBR \n net.core.default_qdisc=fq \n net.ipv4.tcp_congestion_control=bbr \n #开启内核转发 \n net.ipv4.ip_forward=1 \n #优先使用ram \n vm.swappiness=0 \n#TCP优化 \n fs.file-max = 512000 \n net.core.rmem_max = 67108864 \n net.core.wmem_max = 67108864 \n net.core.netdev_max_backlog = 250000 \n net.core.somaxconn = 4096 \n net.ipv4.tcp_syncookies = 1 \n net.ipv4.tcp_tw_reuse = 1 \n net.ipv4.tcp_fin_timeout = 30 \n net.ipv4.tcp_keepalive_time = 1200 \n net.ipv4.ip_local_port_range = 10000 65000 \n net.ipv4.tcp_max_syn_backlog = 8192 \n net.ipv4.tcp_max_tw_buckets = 5000 \n net.ipv4.tcp_fastopen = 3 \n net.ipv4.tcp_mem = 25600 51200 102400 \n net.ipv4.tcp_rmem = 4096 87380 67108864 \n net.ipv4.tcp_wmem = 4096 65536 67108864 \n net.ipv4.tcp_mtu_probing = 1 \n '>/etc/sysctl.conf; \
+
 EOF
 
 [[ "$loaderMode" != "0" ]] && AutoNet='1'
